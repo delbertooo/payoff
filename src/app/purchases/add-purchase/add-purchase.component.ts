0f8c628850c953304376f740afaba7bf001fe2b4
Robert Worgul <robert.worgul@scitotec.de>
2018-07-25 00:06:07 +0200

    Fix validation for calculatedPrice

--- src/app/purchases/add-purchase/add-purchase.component.ts
+++ src/app/purchases/add-purchase/add-purchase.component.ts
@@ -3,7 +3,7 @@ import { MatSnackBar } from '@angular/material';
 import { UsersService } from '../users-service.service';
 import { Observable } from 'rxjs/Observable';
 import { PurchasesService } from '../purchases-service.service';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { evaluateTerm } from './evaluate-term';
 
 @Component({
@@ -31,20 +31,30 @@ export class AddPurchaseComponent implements OnInit {
     const initialPurchaser = this.usersService.loadDefaultPurchaser() || '';
     this.purchaseForm = this.fb.group({
       purchase: ['', Validators.required],
-      price: ['', Validators.required],
-      calculatedPrice: [null, Validators.required],
+      price: ['', this.validCalculatedPrice()],
+      calculatedPrice: [null, Validators.compose([Validators.required, Validators.max(Number.MAX_VALUE)])],
       purchaser: [initialPurchaser, Validators.required],
       participants: [[], Validators.compose([Validators.required, Validators.minLength(1)])],
     });
-    this.purchaseForm.get('price').valueChanges.subscribe(price => this.purchaseForm.patchValue({
-        calculatedPrice: evaluateTerm(price),
-    }));
+    this.purchaseForm.get('price').valueChanges.subscribe(price => {
+      this.calculatedPrice.setValue(evaluateTerm(price));
+      this.purchaseForm.get('price').updateValueAndValidity({ onlySelf: true, emitEvent: false });
+    });
+  }
+
+  private validCalculatedPrice(): ValidatorFn {
+    return (fc: AbstractControl): ValidationErrors | null => {
+      if (!this.purchaseForm) {
+        return null;
+      }
+      return this.calculatedPrice.valid ? null : { invalidCalculatedPrice: true };
+    }
   }
 
   save(): void {
     if (!this.purchaseForm.valid) {
       console.warn('invalid', this.purchaseForm.value)
-      this.snackBar.open('Please check your input data!', undefined, {duration: 2000});
+      this.snackBar.open('Please check your input data!', undefined, { duration: 2000 });
       return;
     }
     const val = this.purchaseForm.value;
@@ -57,7 +67,7 @@ export class AddPurchaseComponent implements OnInit {
       })
       .subscribe(
         () => console.log('redirect to list'),
-        e => this.snackBar.open('An error occured :(', undefined, {duration: 2000})
+        e => this.snackBar.open('An error occured :(', undefined, { duration: 2000 })
       );
   }
 
