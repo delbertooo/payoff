0c5eef4f2423d922df7505321c1dceef5f214f6a
Robert Worgul <robert.worgul@scitotec.de>
2018-10-02 15:06:39 +0200

    Add personal settings page

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonalSettingsComponent } from './personal-settings/personal-settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'personal-settings', component: PersonalSettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { PersonalSettingsComponent } from './personal-settings/personal-settings.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    SharedModule,
   ],
-  declarations: []
+  declarations: [PersonalSettingsComponent]
 })
 export class SettingsModule { }
