ada01c01d366c3038303121ee03fefce9a506c63
Robert Worgul <robert.worgul@scitotec.de>
2019-03-11 01:24:31 +0100

    Cache purchasers for a short while

--- src/app/shared/users-service.service.ts
+++ src/app/shared/users-service.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/defer';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/publishReplay';
+import 'rxjs/add/operator/take';
 import { ApiHttp } from '@app-core';
 
 @Injectable()
 export class UsersService {
-  
+
   private static readonly DEFAULT_PURCHASER_KEY = 'payoffDefaultPurchaser';
+
+  private findPurchasers$: Observable<string[]>;
 
   constructor(
     private http: ApiHttp,
   ) { }
 
   findPurchasers(): Observable<string[]> {
-    return this.http.get('/available-purchasers');
+    if (!this.findPurchasers$) {
+      const ttlMillis = 60000;
+      this.findPurchasers$ = Observable.defer(() => this.http.get('/available-purchasers'))
+        .publishReplay(1, ttlMillis)
+        .refCount()
+        .take(1);
+    }
+    return this.findPurchasers$;
   }
 
   loadDefaultPurchaser(): string {
